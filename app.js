cd1cf1bbc4a423983b55d1e0fd407b348f3a431a
abhishek <aksharma.27mjm@gmail.com>
2023-01-13 02:35:36 +0530

    user schema for signup setup

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ const app = express();
 
 //passports
 
-
+//middlewares
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded("extended: true"));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
